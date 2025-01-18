7240876125cd6c6bbc0391b748a4cf4deedcce76
ValentinHerzog <valentin.herzog@hotmail.com>
2025-01-18 12:04:55 +0100

    Implement player DTO validation, update navigation layout, and add login page functionality

--- Puissance4API/DTO/PlayerDTO.cs
+++ Puissance4API/DTO/PlayerDTO.cs
@@ -1,8 +1,14 @@
+
+
+using System.ComponentModel.DataAnnotations;
+
 namespace Puissance4API.DTO
 {
     public class PlayerDTO
     {
+        [Required]
         public string Login { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
--- Puissance4API/DTO/PlayerResponseDTO.cs
+++ Puissance4API/DTO/PlayerResponseDTO.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Puissance4API.DTO
 {
     public class PlayerResponseDTO
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Login { get; set; }
     }
 }
